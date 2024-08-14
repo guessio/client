e36ba0079725f53896f6cfa4e746d238b4812d71
galileor97 <galwicakso@gmail.com>
2024-08-14 13:35:23 +0700

    Card dynamic and geting the value, done

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,20 +1,16 @@
-import React from 'react'
-import Card from '../components/Card'
+import React from 'react';
+import Card from '../components/Card';
 
 const DashboardPage = () => {
     const cards = Array.from({ length: 50 }, (_, i) => i + 1);
 
-   
-    
     return (
-    
-            <div className="flex grid grid-cols-5 gap-5 m-20">
-                {cards.map((number) => (
-                    <Card num={number}/>
-                ))}
-            </div>
-
-    )
+        <div className="flex grid grid-cols-5 gap-5 m-20 cursor-pointer">
+            {cards.map((number) => (
+                <Card key={number} num={number} />
+            ))}
+        </div>
+    );
 }
 
-export default DashboardPage
+export default DashboardPage;
