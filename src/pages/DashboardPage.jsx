c742eb93e49779e0b5e2bc22d6c3c27e657d7a6a
galileor97 <galwicakso@gmail.com>
2024-08-14 14:58:55 +0700

    Create input component

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import Card from '../components/Card';
+import Input from '../components/Input';
 
 const DashboardPage = () => {
     const cards = Array.from({ length: 50 }, (_, i) => i + 1);
 
     return (
-        <div className="flex grid grid-cols-5 gap-5 m-20 cursor-pointer">
-            {cards.map((number) => (
-                <Card key={number} num={number} />
-            ))}
-        </div>
+        <>
+            <div className='justify-center items-center max-w-full pt-10 mx-auto'>
+
+                <Input />
+                <div className="flex grid lg:grid-cols-5 gap-5 lg:m-20 cursor-pointer">
+                    {cards.map((number) => (
+                        <Card key={number} num={number} />
+                    ))}
+                </div>
+            </div>
+        </>
     );
 }
 
