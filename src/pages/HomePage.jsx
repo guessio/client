7c754f8614b4e4e7d139f2dbed77451fbc8581f3
galileor97 <galwicakso@gmail.com>
2024-08-15 10:02:57 +0700

    Updated dashboard design

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,9 +5,6 @@ import Button from '../components/Button'
 const HomePage = () => {
     return (
         <>
-
-
-        
             <div className='text-center'>
                 <img src={logo} alt="" className='w-1/2 h-1/2 mx-auto -mt-36' />
             </div>
